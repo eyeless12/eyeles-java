7bfce06608dfb68ea5b4ec5cb0b595bf8b7a4289
Max <maxim.chevychelov@gmail.com>
2024-03-02 21:09:51 +0500

    hw3

--- scrapper/src/main/java/edu/java/configuration/ApplicationConfig.java
+++ scrapper/src/main/java/edu/java/configuration/ApplicationConfig.java
@@ -1,16 +1,28 @@
 package edu.java.configuration;
 
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import java.time.Duration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.validation.annotation.Validated;
 
 @Validated
 @ConfigurationProperties(prefix = "app", ignoreUnknownFields = false)
 public record ApplicationConfig(
     @NotNull
-    Scheduler scheduler
+    @Bean
+    Scheduler scheduler,
+
+    @NotNull
+    BaseUrl baseUrl,
+
+    @NotEmpty
+    String localDbPath
 ) {
     public record Scheduler(boolean enable, @NotNull Duration interval, @NotNull Duration forceCheckDelay) {
+    }
+
+    public record BaseUrl(@NotEmpty String github, @NotEmpty String stackoverflow, @NotEmpty String trackerBot) {
     }
 }
