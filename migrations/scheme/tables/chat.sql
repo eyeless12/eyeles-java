d4b670e95b5906468480b7f7a158b89ee0fc310f
Max <maxim.chevychelov@gmail.com>
2024-03-11 02:05:30 +0500

    scheme fixes

--- migrations/scheme/tables/chat.sql
+++ migrations/scheme/tables/chat.sql
@@ -1,5 +1,5 @@
 create table if not exists chat
 (
     id         int primary key generated always as identity,
-    created_at timestamp with time zone not null default CURRENT_TIMESTAMP,
+    created_at timestamp with time zone not null default CURRENT_TIMESTAMP
 );
--- migrations/scheme/tables/chat_link.sql
+++ migrations/scheme/tables/chat_link.sql
@@ -2,5 +2,5 @@ create table if not exists chat_link
 (
     chat_id int not null references chat(id),
     link_id int not null references link(id),
-    primary key (chat_id, link_id),
+    primary key (chat_id, link_id)
 );
