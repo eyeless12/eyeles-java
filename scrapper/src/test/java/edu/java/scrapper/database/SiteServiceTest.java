d417c1efb9ff72ab1cdafbb72096713cc5d2df07
Max <maxim.chevychelov@gmail.com>
2024-04-20 16:06:57 +0500

    hw9

--- scrapper/src/test/java/edu/java/scrapper/database/SiteServiceTest.java
+++ scrapper/src/test/java/edu/java/scrapper/database/SiteServiceTest.java
@@ -1,41 +1,38 @@
 package edu.java.scrapper.database;
 
-import edu.java.service.DomainService;
+import edu.java.service.SiteService;
 import edu.java.util.CommonUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
-public class DomainServiceTest extends BaseDatabaseTest {
+public class SiteServiceTest extends BaseDatabaseTest {
     @Autowired
-    private DomainService domainService;
+    private SiteService siteService;
 
     @Test
     public void testNormalize() {
-        assertThat(domainService.normalizeLink(CommonUtils.toURL(
+        assertThat(siteService.normalizeLink(CommonUtils.toURL(
             "https://github.com/google/googletest/blob/main/googlemock/include/gmock/gmock-cardinalities.h")))
             .isEqualTo("https://github.com/google/googletest");
     }
 
     @Test
     public void validateLinkTest() {
-        assertThat(domainService.validateLink(CommonUtils.toURL(
+        assertThat(siteService.validateLink(CommonUtils.toURL(
             "https://github.com/google/googletest/blob/main/googlemock/include/gmock/gmock-cardinalities.h")))
             .isEmpty();
 
-        assertThat(domainService.validateLink(CommonUtils.toURL("https://github.com"))).isPresent();
+        assertThat(siteService.validateLink(CommonUtils.toURL("https://github.com"))).isPresent();
 
-        assertThat(domainService.validateLink(CommonUtils.toURL(
+        assertThat(siteService.validateLink(CommonUtils.toURL(
             "https://github.com/fessur/Computer-Architecture/blob/main/lab2/testbench.sv")))
             .isPresent();
 
-        assertThat(domainService.validateLink(CommonUtils.toURL("https://stackoverflow.com/questions/12276")))
+        assertThat(siteService.validateLink(CommonUtils.toURL("https://stackoverflow.com/questions/12276")))
             .isPresent();
 
-        assertThat(domainService.validateLink(CommonUtils.toURL(
+        assertThat(siteService.validateLink(CommonUtils.toURL(
             "https://stackoverflow.com/questions/26881739/unable-to-get-spring-boot-to-automatically-create-database-schema")))
             .isEmpty();
     }
